96f0fdd4cdcf706ba761a12156e3b991dbf7e03d
曾谞旺 <zengxuwang@bytedance.com>
2022-01-30 10:56:25 +0800

    :fire: remove uselsss pod files

--- Example/Pods/Target Support Files/BasicKit/BasicKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_BasicKit : NSObject
-@end
-@implementation PodsDummy_BasicKit
-@end
--- Example/Pods/Target Support Files/Pods-BasicKitApp/Pods-BasicKitApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BasicKitApp : NSObject
-@end
-@implementation PodsDummy_Pods_BasicKitApp
-@end
